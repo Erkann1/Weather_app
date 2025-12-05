d3450228a12bc7aff38411641ee8c82b270c8451
Erkan <erkan.ozturk@arcelik.com>
2025-12-05 14:06:36 +0300

    Implement full screen wake up behavior

--- android/app/src/main/java/com/example/weatherannounce/MainActivity.java
+++ android/app/src/main/java/com/example/weatherannounce/MainActivity.java
@@ -1,5 +1,18 @@
 package com.example.weatherannounce;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Ekranı uyandırma ve kilit ekranında gösterme ayarları
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+    }
+}
